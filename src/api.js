c119e15c19e9b577dc93f0519665fc7e41cf0bc4
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2025-06-12 00:18:34 +0530

    feat(api): integrate weather API

---
+++ src/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY;
+const baseWeatherApiUrl = "http://api.weatherapi.com/v1/current.json?key=" + weatherApiKey + "&q=";
+
+export const getWeather = async (location) => {
+  try {
+    console.log(weatherApiKey, "weatherApiKey")
+    if (!weatherApiKey) {
+      throw new Error("WEATHER_API_KEY is not defined. Please set it in your environment variables.");
+    }
+    const response = await axios.get(baseWeatherApiUrl + location);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    throw error;
+  }
+}
