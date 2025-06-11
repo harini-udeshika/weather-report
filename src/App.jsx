2d6668f65c422b3222715afcbe7c14b1e5650dce
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2025-06-12 00:20:00 +0530

    feat: fetch and display current data for Colombo

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { getWeather } from './api'
+import appLogo from './assets/weather-app.png'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [weatherData, setWeatherData] = useState()
+  const location = 'Colombo';
+  useEffect(() => {
+   getWeather(location)
+   .then((data)=>{
+      setWeatherData(data)
+      console.log("Weather data for", location, ":", data)
+   })
+    .catch((error) => {
+        console.error("Error fetching weather data:", error)
+    })
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <img src={appLogo}  className="logo" alt="logo" />
+        <header className="App-header">
+
+          <h1>
+            Simple Weather App
+          </h1>
+          {weatherData ? (
+            <div>
+              <h2>Weather in {location}</h2>
+              <p>Temperature: {weatherData.current.temp_c}°C</p>
+              <p>Condition: {weatherData.current.condition.text}</p>
+              <p>Humidity: {weatherData.current.humidity}</p>
+              <p>Wind speed: {weatherData.current.wind_kph} kph</p>
+              <p>UV index: {weatherData.current.uv}</p>
+            </div>
+          ) : (
+            <p>Loading weather data...</p>
+          )}
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
