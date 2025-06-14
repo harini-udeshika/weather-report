75e7e52ac9c9538c88ff52bcbdfb4b474cec2abe
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2025-06-14 16:41:24 +0530

    fix: change weather api

--- src/api.js
+++ src/api.js
@@ -1,23 +1,32 @@
 import axios from 'axios';
 
 const weatherApiKey = import.meta.env.VITE_WEATHER_API_KEY;
-const ipinfoKey=import.meta.env.VITE_IPINFO_KEY
+const ipinfoKey = import.meta.env.VITE_IPINFO_KEY
 // const googlePlacesApiKey = import.meta.env.VITE_GOOGLE_PLACES_API_KEY;
 const baseWeatherApiUrl = "https://api.weatherapi.com/v1/forecast.json?key=" + weatherApiKey + "&q=";
 
 export const getWeather = async (location) => {
   try {
-    console.log(weatherApiKey, "weatherApiKey")
+    console.log(weatherApiKey, "weatherApiKey");
     if (!weatherApiKey) {
       throw new Error("WEATHER_API_KEY is not defined. Please set it in your environment variables.");
     }
-    const response = await axios.get(baseWeatherApiUrl + location);
-    return response.data;
+
+    const response = await fetch(`${baseWeatherApiUrl}${encodeURIComponent(location)}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+
   } catch (error) {
     console.error("Error fetching weather data:", error);
     throw error;
   }
-}
+};
+
 
 export const getLocation = async () => {
   try {
